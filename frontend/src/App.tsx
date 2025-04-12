1eb8d4e3f5043063621bb4e35d8237f9da487121
Artem-Brui <artembryj@gmail.com>
2025-04-12 12:26:29 +0200

    add initial folders, create router in frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Header</h1>
+      <Outlet/>
+      <h1>Footer</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/index.tsx
@@ -0,0 +1,18 @@
+
+import { createRoot } from "react-dom/client";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./index.scss";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="dashboard" element={<h1>dashboard</h1>} />
+        <Route path="add" element={<h1>add</h1>} />
+        <Route path="edit" element={<h1>edit</h1>} />
+        <Route index element={<Navigate to="/dashboard" />} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
