d557d4aa43542bc14b2d2ff24c702051e45839bb
Artem-Brui <artembryj@gmail.com>
2025-04-16 19:03:07 +0200

    updated

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import "./index.scss";
 import App from "./App.tsx";
 import Dashboard from "./components/BooksTable/BooksTable.tsx";
